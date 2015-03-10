88b3300b2a3bf123f68c64255ab6f9916ee4a081
Joelikho <joelikho.13@gmail.com>
2015-03-10 13:12:01 +0100

    solucionat el problema els salts de linea ex4 funciona perfectament

--- exercici4/TestClient.java
+++ exercici4/TestClient.java
@@ -10,7 +10,7 @@ public class TestClient {
      public void testClientInforme() {
          // comprova que el constructor Punt() pugui ser cridat
         Client c=new Client("53639767","Joel","65847554");
-        String s="Informe de lloguers del client Joel (53639767)\nTata Vista: 90.0e\nImport a pagar: 90.0e\nPunts guanyats: 1";
+        String s="Informe de lloguers del client Joel (53639767)\n\tTata Vista: 90.0e\nImport a pagar: 90.0e\nPunts guanyats: 1\n";
         Vehicle vehicleBasic = new Vehicle("Tata", "Vista", Vehicle.BASIC);
         // demostració de construccuó d'un lloguer amb una data
         SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
