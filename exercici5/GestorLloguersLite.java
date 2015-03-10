84c3ce503b40feaa2033038f0e2563122f988244
Joelikho <joelikho.13@gmail.com>
2015-03-10 13:41:38 +0100

    exercici 5 acabat

--- exercici5/GestorLloguersLite.java
+++ exercici5/GestorLloguersLite.java
@@ -18,19 +18,26 @@ public class GestorLloguersLite{
 		return cli+llo;
 	}
 	public static void main(String[] args) {
-		Client c=new Client("53639767","Joel","65847554");
-		// demostració de construcció d'un vehicle de categoria BASIC
-		Vehicle vehicleBasic = new Vehicle("Tata", "Vista", Vehicle.BASIC);
-		// demostració de construccuó d'un lloguer amb una data
-		SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
-		Date date=null;
-		try{
-			date = dateFormat.parse("2/8/2013");
-		}catch(ParseException e){
-			System.out.println(e);
-		}
-		Lloguer lloguerBasic = new Lloguer(date, 2, vehicleBasic);
-		c.afegeix(lloguerBasic);
+		 // comprova que el constructor Punt() pugui ser cridat
+        Client c=new Client("53639767","Joel","65847554");
+        String s="Informe de lloguers del client Joel (53639767)\n\tTata Vista: 90.0e\n\tFerrari TestaRosa: 180.0e\nImport a pagar: 270.0e\nPunts guanyats: 2\n";
+        Vehicle vehicleBasic = new Vehicle("Tata", "Vista", Vehicle.BASIC);
+        Vehicle vehicleGeneral = new Vehicle("Audi", "A3", Vehicle.GENERAL);
+        Vehicle vehicleLuxe= new Vehicle("Ferrari","TestaRosa",Vehicle.LUXE);
+        // demostració de construccuó d'un lloguer amb una data
+        SimpleDateFormat dateFormat = new SimpleDateFormat("d/M/yyyy");
+        Date date=null;
+        try{
+            date = dateFormat.parse("2/8/2013");
+        }catch(ParseException e){
+            System.out.println(e);
+        }
+        Lloguer lloguerLuxe = new Lloguer(date, 1, vehicleLuxe);
+        Lloguer lloguerGeneral = new Lloguer(date, 1, vehicleGeneral);
+        Lloguer lloguerBasic = new Lloguer(date, 1, vehicleBasic);
+        c.afegeix(lloguerBasic);
+        c.afegeix(lloguerGeneral);
+        c.afegeix(lloguerLuxe); 
 		System.out.println(c.informe());
 		System.out.println(c.informeHTML());
 	}
